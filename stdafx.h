bc7a1696a9e01dd5a22a4e65397c05e097b4e56a
mmmaolololo <48906848+mmmaolololo@users.noreply.github.com>
2019-03-25 17:09:40 +0800

    Create stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,22 @@
+
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN            
+
+#include <windows.h>
+
+
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#include<cctype>
+#include<array>
+#include<functional>
+#include<string>
+#include<fstream>
+#include<vector>//miss this one for a while,but it still works,magic.
